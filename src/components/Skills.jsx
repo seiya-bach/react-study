0c9c0dea6dec2ff16a3dfa86e84b440d4eaffedd
ogawa.seiya <ogawa.seiya.pri@gmail.com>
2022-12-12 01:26:23 +0900

    Section 5-2まで完了

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const Skills = () => {
+    const [languageList, setLanguageList] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://api.github.com/users/seiya-bach/repos').then((response) => {
+            const languageList = response.data.map((res) => res.language);
+            const countedLanguageList = generateLanguageCountObj(languageList);
+            setLanguageList(countedLanguageList);
+        });
+    }, []);
+
+    const generateLanguageCountObj = (allLanguageList) => {
+        const notNullLanguageList = allLanguageList.filter((language) => language != null);
+        const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+        return uniqueLanguageList.map((item) => {
+            return {
+                language: item,
+                count: allLanguageList.filter((language) => language === item).length,
+            };
+        });
+    };
+
+    console.log(languageList);
+
     return (
         <div id='skills'>
             <div className='container'>
