dfd7ebc030b3575405a76914c2452203a468fa42
Seiya <ogawa.seiya.pri@gmail.com>
2022-12-13 01:34:32 +0900

    Section 5 (#3)
    * Section 5-2まで完了
    
    * 5-5まで

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,66 @@
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
+import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
+import { requestStates } from '../constants';
+import Circle from 'react-circle';
+
 const Skills = () => {
+    const [state, dispatch] = useReducer(skillReducer, initialState);
+
+    useEffect(() => {
+        dispatch({ type: actionTypes.fetch });
+        axios
+            .get('https://api.github.com/users/seiya-bach/repos')
+            .then((response) => {
+                const languageList = response.data.map((res) => res.language);
+                const countedLanguageList = generateLanguageCountObj(languageList);
+                dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+            })
+            .catch(() => {
+                dispatch({ type: actionTypes.error });
+            });
+    }, []);
+
+    const generateLanguageCountObj = (allLanguageList) => {
+        const notNullLanguageList = allLanguageList.filter((language) => language != null);
+        const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+        return uniqueLanguageList.map((item) => {
+            return {
+                language: item,
+                count: allLanguageList.filter((language) => language === item).length,
+            };
+        });
+    };
+
+    const converseCountToPercentage = (count) => {
+        if (count > 10) {
+            return 100;
+        }
+        return count * 10;
+    };
+
+    const sortedLanguageList = () => state.languageList.sort((firstLang, nextLang) => nextLang.count - firstLang.count);
+
     return (
         <div id='skills'>
             <div className='container'>
                 <div className='heading'>
                     <h2>Skills</h2>
                 </div>
-                <div className='skills-container'></div>
+                <div className='skills-container'>
+                    {state.requestState === requestStates.loading && <p className='description'>取得中...</p>}
+                    {state.requestState === requestStates.error && <p className='description'>エラーが発生しました</p>}
+                    {state.requestState === requestStates.success &&
+                        sortedLanguageList().map((item, index) => (
+                            <div key={index} className='skill-item'>
+                                <p className='description'>
+                                    <strong>{item.language}</strong>
+                                </p>
+                                <Circle animate progress={converseCountToPercentage(item.count)} />
+                            </div>
+                        ))}
+                </div>
             </div>
         </div>
     );
