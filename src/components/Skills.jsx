e88adeed9077a2994435e9c2914c110d18f82fbf
Seiya <ogawa.seiya.pri@gmail.com>
2022-12-14 00:24:37 +0900

    section5-5まで完了 (#5)

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,46 +1,9 @@
-import { useEffect, useReducer } from 'react';
-import axios from 'axios';
-import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
 import { requestStates } from '../constants';
 import Circle from 'react-circle';
+import { useSkills } from '../customHooks/useSkills';
 
 const Skills = () => {
-    const [state, dispatch] = useReducer(skillReducer, initialState);
-
-    useEffect(() => {
-        dispatch({ type: actionTypes.fetch });
-        axios
-            .get('https://api.github.com/users/seiya-bach/repos')
-            .then((response) => {
-                const languageList = response.data.map((res) => res.language);
-                const countedLanguageList = generateLanguageCountObj(languageList);
-                dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
-            })
-            .catch(() => {
-                dispatch({ type: actionTypes.error });
-            });
-    }, []);
-
-    const generateLanguageCountObj = (allLanguageList) => {
-        const notNullLanguageList = allLanguageList.filter((language) => language != null);
-        const uniqueLanguageList = [...new Set(notNullLanguageList)];
-
-        return uniqueLanguageList.map((item) => {
-            return {
-                language: item,
-                count: allLanguageList.filter((language) => language === item).length,
-            };
-        });
-    };
-
-    const converseCountToPercentage = (count) => {
-        if (count > 10) {
-            return 100;
-        }
-        return count * 10;
-    };
-
-    const sortedLanguageList = () => state.languageList.sort((firstLang, nextLang) => nextLang.count - firstLang.count);
+    const [sortedLanguageList, fetchRequestState, converseCountToPercentage] = useSkills();
 
     return (
         <div id='skills'>
@@ -49,9 +12,9 @@ const Skills = () => {
                     <h2>Skills</h2>
                 </div>
                 <div className='skills-container'>
-                    {state.requestState === requestStates.loading && <p className='description'>取得中...</p>}
-                    {state.requestState === requestStates.error && <p className='description'>エラーが発生しました</p>}
-                    {state.requestState === requestStates.success &&
+                    {fetchRequestState === requestStates.loading && <p className='description'>取得中...</p>}
+                    {fetchRequestState === requestStates.error && <p className='description'>エラーが発生しました</p>}
+                    {fetchRequestState === requestStates.success &&
                         sortedLanguageList().map((item, index) => (
                             <div key={index} className='skill-item'>
                                 <p className='description'>
